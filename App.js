41027ed1abf40bde25390d6e3e4cbd167a7e5e04
Lior Kastenbaum <liorkasti@gmail.com>
2021-11-16 00:52:32 +0200

    Add axios get user posts from api and react-native-paper and moment

--- App.js
+++ App.js
@@ -6,107 +6,168 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, Image, FlatList, Pressable, Modal, } from 'react-native';
+import axios from 'axios';
+import { Searchbar, Card, Title } from 'react-native-paper';
+import moment from 'moment';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState < boolean > (false);
+  const [userID, setUserID] = useState < string > ('');
+  const [posts, setPosts] = useState < Array > ([]);
+  const [requestStatus, setRequestStatus] = useState < string > ('');
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyleColor = isDarkMode ? 'black' : 'white';
+  const textStyleColor = isDarkMode ? 'white' : 'black';
+
+  const getUserPosts = async () => {
+    setRequestStatus('');
+    try {
+      const response = await axios.get(`https://api.stackexchange.com/2.2/users/${userID}/questions?order=desc&sort=creation&site=stackoverflow`);
+      const result = await response.data;
+      setPosts(result.items);
+      console.log('posts :>> ', posts);
+    } catch (error) {
+      console.error(`user id:${userID} error ` + error);
+      setRequestStatus('fail');
+    }
+  };
+
+  const renderItem = ({ item }) => {
+    console.log('item :>> ', item);
+    const date = new Date(item.creation_date * 1000).toLocaleDateString("en-US")
+    const answers = item.answer_count;
+    const viewCount = item.view_count;
+
+    return (
+      <Card style={[styles.card, { backgroundColor: backgroundStyleColor, borderBottomColor: textStyleColor }]}>
+        <Card.Content>
+          <View style={styles.cardTitle}>
+            <Title style={{ color: textStyleColor }} color={backgroundStyleColor}>{item.title}</Title>
+          </View>
+          <View style={styles.cardSubTitle}>
+            <Text style={{ color: textStyleColor }}>
+              {date ? `${(moment(date).fromNow())}` : ``}
+              {answers ? `  Answers: ${answers}` : ``}
+              {viewCount ? `  Viewed: ${viewCount} times` : ``}</Text>
+          </View>
+        </Card.Content>
+      </Card>
+    );
+  }
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <View style={[styles.background, { backgroundColor: backgroundStyleColor }]}>
+      <Text style={[styles.title, { color: textStyleColor }]}>
+        Get Stack Overflow Posts
       </Text>
-      <Text
+      <Searchbar
         style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+          styles.textInput,
+          { borderColor: textStyleColor, color: textStyleColor },
+        ]}
+        placeholder={'Enter Stack Overflow User ID'}
+        placeholderTextColor={isDarkMode ? textStyleColor : 'lightgray'}
+        value={userID}
+        onChangeText={setUserID}
+        blurOnSubmit={true}
+        onSubmitEditing={getUserPosts}
+      />
+      {posts[0]?.owner &&
+        <>
+          <FlatList
+            data={posts}
+            renderItem={renderItem}
+            keyExtractor={item => item.question_id}
+            ListFooterComponent={() => <View style={{ height: 100 }} />}
+            style={{ marginTop: 10 }}
+          />
+        </>
+      }
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  background: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  switchContainer: {
+    flexDirection: 'row-reverse',
+    padding: '2%',
+    fontWeight: '300',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  icon: {
+    color: '#fff',
+    textAlign: 'right',
+    fontSize: 20
   },
+  title: {
+    textAlign: 'center',
+    marginTop: '5%',
+    fontSize: 20,
+  },
+  textInput: {
+    width: '65%',
+    borderRadius: 10,
+    borderBottomWidth: 2,
+    margin: '5%',
+    elevation: 5,
+
+  },
+  userDetailsContainer: {
+    flexDirection: 'row',
+    width: '65%',
+    marginTop: '5%',
+    justifyContent: 'space-between',
+  },
+  sortTitle: {
+    textAlign: 'center',
+    marginTop: '5%',
+    fontWeight: '500',
+    marginBottom: '2%',
+  },
+  card: {
+    flex: 1,
+    position: 'relative',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    width: '90%',
+    borderBottomWidth: 2,
+    borderRadius: 10,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    paddingVertical: 10,
+    paddingRight: 10,
+    alignItems: 'center',
+    elevation: 5,
+  },
+  cardContent: {
+    width: '80%',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+  cardTitle: {
+    width: "75%",
+    height: 'auto',
+    fontSize: 16,
+    fontWeight: '300',
+    justifyContent: 'flex-start',
+  },
+  cardSubTitle: {
+    flexDirection: 'row',
+    width: '90%',
+    height: 20,
+    fontWeight: '300',
+    justifyContent: 'flex-start'
+  }
 });
 
 export default App;
